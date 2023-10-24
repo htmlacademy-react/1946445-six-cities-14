6fe55ca77c171d6ccc0dcde7bd36ad244bcd8109
Immolin <serebrena@mail.ru>
2023-10-25 02:34:22 +1000

    Маршрут по тайным тропам

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
-export {default as Favorites} from '../pages/Favorites/Favorites';
-export {default as Login} from '../pages/Login/Login';
-export {default as MainPage} from '../pages/Main/Main';
-export {default as NotFound} from '../pages/Not-Found/Not-Found';
-export {default as Offer} from '../pages/Offer/Offer';
+export {default as Favorites} from './Favorites/Favorites';
+export {default as Login} from './Login/Login';
+export {default as MainPage} from './Main/Main';
+export {default as NotFound} from './Not-Found/Not-Found';
+export {default as Offer} from './Offer/Offer';
 
